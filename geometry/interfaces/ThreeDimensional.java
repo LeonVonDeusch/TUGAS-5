86c16afb2173d6956a471d04341e2b94b9db0f44
LeonVonDeusch <dandyzikria@gmail.com>
2025-04-22 14:21:30 +0700

    Delete geometry directory

--- geometry/interfaces/ThreeDimensional.java
+++
@@ -1,6 +0,0 @@
-package praktic.geometry.interfaces;
-
-public interface ThreeDimensional {
-    double getSurfaceArea(); // menghitung luas permukaan
-    double getVolume(); // menghitung volume
-}
--- geometry/interfaces/TwoDimensional.java
+++
@@ -1,6 +0,0 @@
-package praktic.geometry.interfaces;
-
-public interface TwoDimensional {
-    double getArea(); // menghitung luas
-    double getPerimeter(); // menghitung keliling
-}
